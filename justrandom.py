aba6003cf35b94436cb4704ac3378cc23997cf22
gabiwp1 <gabrielmanciu765@gmail.com>
2026-08-04 14:45:02 +0000

    Update justrandom.py

--- justrandom.py
+++ justrandom.py
@@ -31,7 +31,7 @@ def getInformation(url):
 
     headers = {
         'User-Agent':(
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
             'AppleWebKit/537.36 '
             '(KHTML, like Gecko) '
             'Chrome/120.0.0.0 Safari/537.36 '
